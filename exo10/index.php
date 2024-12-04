a498c9014e489678e28a6ebdb2ca9d7a9be557eb
aliii390 <alli.said.errahmani@gmail.com>
2024-12-04 12:40:06 +0100

    fin de toute les partie

---
+++ exo10/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$tableau = [02=> "Aisne", 59 => "Nord", 60 => "l'Oise", 62 => "le Pas-de-Calais", 80 => "la Somme", 51 => "Reims"];
+
+
+
+foreach($tableau as $codeDepartement => $nomDepartement){
+
+    echo "<br> Le département: $codeDepartement; <br>   Le nom du département: $nomDepartement <br> ";
+}
+
+
+
+
+?>
